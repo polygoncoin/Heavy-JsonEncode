f656c068f7ac73cfa6269ea32403add03e74f1a1
“polygoncoin <polygon.co.in@gmail.com>
2024-06-18 20:18:02 +0530

    JsonEncode

---
+++ JsonEncode.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ * Creates JSON
+ *
+ * This class is built to avoid creation of large array objects
+ * (which leads to memory limit issues for larger data set)
+ * which are then converted to JSON. This class gives access to
+ * create JSON in parts for what ever smallest part of data
+ * we have of the large data set which are yet to be fetched.
+ *
+ * @category   JSON
+ * @package    JsonEncode
+ * @author     Ramesh Narayan Jangid
+ * @copyright  Ramesh Narayan Jangid
+ * @version    Release: @1.0.0@
+ * @since      Class available since Release 1.0.0
+ */
+class JsonEncode
+{
+    /**
+     * Temporary Stream
+     *
+     * @var string
+     */
+    private $tempStream = '';
+
+    /**
+     * Array of JsonEncodeObject objects
+     *
+     * @var array
+     */
+    private $objects = [];
+
+    /**
+     * Current JsonEncodeObject object
+     *
+     * @var object
+     */
+    private $currentObject = null;
+
+    /**
+     * JsonEncode constructor
+     */
+    public function __construct()
+    {
+        ob_start();
+        $this->tempStream = fopen("php://temp", "w+b");
+    }
+
+    /**
+     * Write to temporary stream
+     * 
+     * @return void
+     */
+    public function write($str)
+    {
+        fwrite($this->tempStream, $str);
+    }
+
+    /**
+     * Escape the json string key or value
+     *
+     * @param string $str json key or value string.
+     * @return string
+     */
+    private function escape($str)
+    {
+        if (is_null($str)) return 'null';
+        $escapers = array("\\", "/", "\"", "\n", "\r", "\t", "\x08", "\x0c");
+        $replacements = array("\\\\", "\\/", "\\\"", "\\n", "\\r", "\\t", "\\f", "\\b");
+        $str = str_replace($escapers, $replacements, $str);
+        return '"' . $str . '"';
+    }
+
+    /**
+     * Encodes both simple and associative array to json
+     *
+     * @param $arr string value escaped and array value json_encode function is applied.  
+     * @return void
+     */
+    public function encode($arr)
+    {
+        if ($this->currentObject) {
+            $this->write($this->currentObject->comma);
+        }
+        if (is_array($arr)) {
+            $this->write(json_encode($arr));
+        } else {
+            $this->write($this->escape($arr));
+        }
+        if ($this->currentObject) {
+            $this->currentObject->comma = ',';
+        }
+    }
+
+    /**
+     * Add simple array/value as in the json format.
+     *
+     * @param $value data type is string/array. This is used to add value/array in the current Array.
+     * @return void
+     */
+    public function addValue($value)
+    {
+        if ($this->currentObject->mode !== 'Array') {
+            throw new Exception('Mode should be Array');
+        }
+        $this->encode($value);
+    }
+
+    /**
+     * Add simple array/value as in the json format.
+     *
+     * @param string $key   key of associative array
+     * @param        $value data type is string/array. This is used to add value/array in the current Array.
+     * @return void
+     */
+    public function addKeyValue($key, $value)
+    {
+        if ($this->currentObject->mode !== 'Object') {
+            throw new Exception('Mode should be Object');
+        }
+        $this->write($this->currentObject->comma);
+        $this->write($this->escape($key) . ':');
+        $this->currentObject->comma = '';
+        $this->encode($value);
+    }
+
+    /**
+     * Start simple array
+     *
+     * @param string $key Used while creating simple array inside an associative array and $key is the key.
+     * @return void
+     */
+    public function startArray($key = null)
+    {
+        if ($this->currentObject) {
+            $this->write($this->currentObject->comma);
+            array_push($this->objects, $this->currentObject);
+        }
+        $this->currentObject = new JsonEncodeObject('Array');
+        if (!is_null($key)) {
+            $this->write($this->escape($key) . ':');
+        }
+        $this->write('[');
+    }
+
+    /**
+     * End simple array
+     *
+     * @return void
+     */
+    public function endArray()
+    {
+        $this->write(']');
+        $this->currentObject = null;
+        if (count($this->objects)>0) {
+            $this->currentObject = array_pop($this->objects);
+            $this->currentObject->comma = ',';
+        }
+    }
+
+    /**
+     * Start simple array
+     *
+     * @param string $key Used while creating associative array inside an associative array and $key is the key.
+     * @return void
+     */
+    public function startObject($key = null)
+    {
+        if ($this->currentObject) {
+            if ($this->currentObject->mode === 'Object' && is_null($key)) {
+                throw new Exception('Object inside an Object should be supported with a Key');
+            }
+            $this->write($this->currentObject->comma);
+            array_push($this->objects, $this->currentObject);
+        }
+        $this->currentObject = new JsonEncodeObject('Object');
+        if (!is_null($key)) {
+            $this->write($this->escape($key) . ':');
+        }
+        $this->write('{');
+    }
+
+    /**
+     * End associative array
+     *
+     * @return void
+     */
+    public function endObject()
+    {
+        $this->write('}');
+        $this->currentObject = null;
+        if (count($this->objects)>0) {
+            $this->currentObject = array_pop($this->objects);
+            $this->currentObject->comma = ',';
+        }
+    }
+
+    /**
+     * Stream Json String.
+     *
+     * @return void
+     */
+    private function streamJson()
+    {
+        if (empty(ob_get_contents())) {
+            // end the json
+            // rewind the temp stream.
+            rewind($this->tempStream);
+            // stream the temp to output
+            $outputStream = fopen("php://output", "w+b");
+            stream_copy_to_stream($this->tempStream, $outputStream);
+            fclose($outputStream);
+        }
+        fclose($this->tempStream);
+    }
+
+    /**
+     * Checks json was properly closed.
+     *
+     * @return void
+     */
+    public function end()
+    {
+        while ($this->currentObject && $this->currentObject->mode) {
+            switch ($this->currentObject->mode) {
+                case 'Array':
+                    $this->endArray();
+                    break;
+                case 'Object':
+                    $this->endObject();
+                    break;
+            }
+        }
+        $this->streamJson();
+    }
+
+    /** 
+     * destruct functipn 
+     */ 
+    public function __destruct() 
+    { 
+        $this->end();
+    }
+}
+
+/**
+ * JSON Encode Object
+ *
+ * This class is built to help maintain state of simple/associative array
+ *
+ * @category   JSON
+ * @package    JsonEncode
+ * @author     Ramesh Narayan Jangid
+ * @copyright  Ramesh Narayan Jangid
+ * @version    Release: @1.0.0@
+ * @since      Class available since Release 1.0.0
+ */
+class JsonEncodeObject
+{
+    public $mode = '';
+    public $comma = '';
+
+    /**
+     * Constructor
+     *
+     * @param string $mode Values can be one among Array/Object
+     */
+    public function __construct($mode)
+    {
+        $this->mode = $mode;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# JsonEncode
+# JSON Encode
+ 
 PHP JSON Encode large data with lesser resources
+ 
+
+## Examples
+ 
+
+### Generating single row output as object.
+ 
+
+    <?php
+    require "JsonEncode.php";
+    
+    // Create JsonEncode Object.
+    $jsonEncodeObj = new JsonEncode();
+    
+    // Execute DB Query
+    $stmt = $db->select($sql);
+    $stmt->execute($params);
+    
+    // For single row - one
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $jsonEncode->encode($row);
+    
+    // For single row - two
+    $jsonEncode->startObject();
+    foreach($stmt->fetch(PDO::FETCH_ASSOC) as $key => $value) {
+        $jsonEncode->addKeyValue($key, $value);
+    }
+    $jsonEncode->endObject();
+    
+    // Free statement resources and close the cursor.
+    $stmt->closeCursor();
+    
+    $jsonEncode = null;
+
+### Generating many rows output as array of objects.
+ 
+
+    <?php
+    require "JsonEncode.php";
+    
+    // Create JsonEncode Object.
+    $jsonEncodeObj = new JsonEncode();
+    
+    // Execute DB Query
+    $stmt = $db->select($sql);
+    $stmt->execute($params);
+    
+    // For multiple rows
+    $jsonEncode->startArray();
+    for(;$row=$stmt->fetch(PDO::FETCH_ASSOC);) {
+        $jsonEncode->encode($row);
+    }
+    $jsonEncode->endArray();
+    
+    // Free statement resources and close the cursor.
+    $stmt->closeCursor();
+    
+    $jsonEncode = null;
+
+### Generating many rows output inside object.
+ 
+
+    <?php
+    require "JsonEncode.php";
+    
+    // Create JsonEncode Object.
+    $jsonEncodeObj = new JsonEncode();
+    
+    // Start JSON object
+    $jsonEncode->startObject();
+    
+    // Execute DB Query - 1
+    $stmt = $db->select($sql);
+    $stmt->execute($params);
+    foreach($stmt->fetch(PDO::FETCH_ASSOC) as $key => $value) {
+        $jsonEncode->addKeyValue($key, $value);
+    }
+    // Free statement resources and close the cursor.
+    $stmt->closeCursor();
+    
+    // Start JSON array inside object
+    $jsonEncode->startArray($assocKey);
+    
+    // Execute DB Query - 2
+    $stmt = $db->select($sql_2);
+    $stmt->execute($params_2);
+    for(;$row=$stmt->fetch(PDO::FETCH_ASSOC);) {
+        $jsonEncode->encode($row);
+    }
+    // Free statement resources and close the cursor.
+    $stmt->closeCursor();
+    
+    // End JSON array inside object
+    $jsonEncode->endArray();
+    
+    // End JSON object
+    $jsonEncode->endObject();
+    
+    $jsonEncode = null;
+
+> The $jsonEncode = null; will stream the generated JSON.
