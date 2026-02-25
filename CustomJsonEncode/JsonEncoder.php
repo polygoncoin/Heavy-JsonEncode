72c58ea9ebc34310def75ce04f025f6c9d430979
“polygoncoin <polygon.co.in@gmail.com>
2026-02-25 20:12:11 +0530

    Readme

--- CustomJsonEncode/JsonEncoder.php
+++ CustomJsonEncode/JsonEncoder.php
@@ -6,8 +6,8 @@
  *
  * @category  JsonEncode
  * @package   CustomJsonEncode
- * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
- * @copyright 2025 Ramesh N Jangid
+ * @author    Ramesh N. Jangid (Sharma) <polygon.co.in@gmail.com>
+ * @copyright © 2026 Ramesh N. Jangid (Sharma)
  * @license   MIT https://opensource.org/license/mit
  * @link      https://github.com/polygoncoin/Microservices
  * @since     Class available since Release 1.0.0
@@ -23,8 +23,8 @@ use CustomJsonEncode\JsonEncode;
  *
  * @category  JsonEncode
  * @package   CustomJsonEncode
- * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
- * @copyright 2025 Ramesh N Jangid
+ * @author    Ramesh N. Jangid (Sharma) <polygon.co.in@gmail.com>
+ * @copyright © 2026 Ramesh N. Jangid (Sharma)
  * @license   MIT https://opensource.org/license/mit
  * @link      https://github.com/polygoncoin/Microservices
  * @since     Class available since Release 1.0.0
--- README.md
+++ README.md
@@ -188,3 +188,19 @@ $jsonEncode = null;
 ```
 
 > The $jsonEncode = null; will stream the generated JSON.
+
+## 🤝 Contributing
+
+Issues and feature requests are welcome.<br />
+Feel free to share them on [issues page](https://github.com/polygoncoin/Heavy-JsonEncode/issues)
+
+## Author
+
+👤 **Ramesh N. Jangid (Sharma)**
+
+- Github: [@polygoncoin](https://github.com/polygoncoin)
+
+## 📝 License
+
+Copyright © 2026 [Ramesh N. Jangid (Sharma)](https://github.com/polygoncoin).<br />
+This project is [MIT](LICENSE) licensed.
